4ceaa7267611706a45978ad36ee2fd88dce8c3c6
rajkumar <raj4122002@gmail.com>
2025-07-25 22:27:46 +0530

    first

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Task = require('./task');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get('/api/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/api/tasks', async (req, res) => {
+  const task = new Task({ text: req.body.text });
+  await task.save();
+  res.json(task);
+});
+
+app.delete('/api/tasks/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(5000, () => console.log('Server running on http://localhost:5000'));
